d26150a249689f962b7109a26ce91b28803d4642
Yaroslav Lisovsky <iyariki.ya@gmail.com>
2017-05-06 23:02:42 +0300

    shooter detect attackers ahed himself

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -7,15 +7,61 @@ public class Shooter : MonoBehaviour
     public GameObject gun;
     private GameObject projectileParent;
     private const string PROJECTILE_PARENT_NAME = "Projectiles";
+    private Spawner myLaneSpawner;
+    private Animator animator;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
+
         projectileParent = FindProjectileParent();
         if (projectileParent == null)
         {
             projectileParent = new GameObject(PROJECTILE_PARENT_NAME);
         }
+
+        SetMyLaneSpawner();
     }
+
+    void Update()
+    {
+        if(animator)
+            animator.SetBool("isAttacking", IsAttackerInLane());
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner == null || myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+
+        foreach (Transform attacker in myLaneSpawner.transform)
+        {
+            if (attacker.position.x > transform.position.x)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private void SetMyLaneSpawner()
+    {
+        Spawner[] spawners = GameObject.FindObjectsOfType<Spawner>();
+
+        foreach (var spawner in spawners)
+        {
+            if (spawner.transform.position.y == transform.position.y)
+            {
+                myLaneSpawner = spawner;
+                break;
+            }
+        }
+    }
+
+
 
     private GameObject FindProjectileParent()
     {
